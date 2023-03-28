ae63fd4ead1eaab796559c97d27f4a1929f4878c
bi9potato <bi9potato>
2023-03-28 15:15:32 -0700

    src code

---
+++ src/main/java/com/wxy/ssm/blog/dto/UploadFileVO.java
@@ -0,0 +1,12 @@
+package com.wxy.ssm.blog.dto;
+
+import lombok.Data;
+
+@Data
+public class UploadFileVO {
+
+    private String src;
+
+    private String title;
+
+}
---
+++ src/main/java/com/wxy/ssm/blog/service/OptionsService.java
@@ -0,0 +1,26 @@
+package com.wxy.ssm.blog.service;
+
+import com.wxy.ssm.blog.entity.Options;
+
+public interface OptionsService {
+    /**
+     * 获得基本信息
+     *
+     * @return 系统设置
+     */
+    Options getOptions();
+
+    /**
+     * 新建基本信息
+     * 
+     * @param options 系统设置
+     */
+    void insertOptions(Options options);
+
+    /**
+     * 更新基本信息
+     * 
+     * @param options 系统设置
+     */
+    void updateOptions(Options options);
+}
